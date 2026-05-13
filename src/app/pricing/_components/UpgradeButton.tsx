6ba9f07f3b20d6d969d35598b210492a4525ecf0
Shourya-B07 <shouryabhardwaj7@gmail.com>
2026-05-13 15:45:46 +0530

    Update pricing page and checkout URL

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function UpgradeButton() {
   const CHEKOUT_URL =
-    "https://codeharbor01.lemonsqueezy.com/buy/51e5bf98-0e45-42af-831d-42b4c95958b0";
+    "https://codeharbor01.lemonsqueezy.com/checkout/buy/51e5bf98-0e45-42af-831d-42b4c95958b0";
 
   return (
     <Link
